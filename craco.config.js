1cdecca6d89321e304b3892df3584c3f5e85726f
Jclass100 <wattont@live.com>
2022-10-08 18:15:36 +1000

    moving away from craco

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-    style: {
-        postcssOptions: {
-            plugins: [
-                require("@tailwindcss/postcss7-compat"),
-                require("autoprefixer"),
-            ],
-        },
-    },
+	style: {
+		postcssOptions: {
+			plugins: [require("@tailwindcss/postcss7-compat")],
+		},
+	},
 };
